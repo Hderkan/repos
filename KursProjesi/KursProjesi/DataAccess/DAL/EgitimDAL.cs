820a88abe1e82b1899d4f8b7f7ead1bd022910b0
hakan derkan <hakanderkan@outlook.com>
2022-03-03 15:13:52 +0300

    düzenle

---
+++ KursProjesi/KursProjesi/DataAccess/DAL/EgitimDAL.cs
@@ -0,0 +1,65 @@
+﻿using KursProjesi.DataAccess.BaglantiDAL;
+using KursProjesi.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KursProjesi.DataAccess.DAL
+{
+    public class EgitimDAL
+    {
+        Kurs kurs = null;
+
+        //burada crud islemlerimizi yapıcaz
+        public List<Kurs> GetALL()
+        {
+              List<Kurs> kurslar = new List<Kurs>();
+            try
+            {
+
+                using (SqlCommand cmd = new SqlCommand($"Select * from tblEgitimler", Baglanti.BaglantiNesnesi))
+                {
+                    Baglanti.Ac();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                               kurs = new Kurs()
+                            {
+                                ID = Convert.ToInt32(dr["ID"]),
+                                Ad = dr["Ad"].ToString(),
+                                Sorumlu = dr["Sorumlu"].ToString(),
+                                BaslangıcTarihi = Convert.ToDateTime(dr["BaslangıcTarihi"]),
+                                Sure = Convert.ToInt32(dr["Sure"])
+
+                            };
+                            kurslar.Add(kurs);
+                        }
+
+                    }
+
+                }
+
+            }
+
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+            finally
+            {
+                Baglanti.Kapa();
+            
+            }
+
+
+        }
+
+    }
+}
---
+++ KursProjesi/KursProjesi/Entity/Kurs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursProjesi.Entity
+{
+    class Kurs
+    {
+        public int ID { get; set; }
+        public string Ad { get; set; }
+        public string Sorumlu { get; set; }
+        public DateTime BaslangıcTarihi { get; set; }
+        public int Sure { get; set; }
+
+    }
+}
