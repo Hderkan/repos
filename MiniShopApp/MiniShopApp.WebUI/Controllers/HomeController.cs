793674fe3e0149ffb5026b845df5c95bd901eb6a
Hderkan <hakanderkan@outlook.com>
2022-05-09 15:12:17 +0300

    düzenlendi

---
+++ MiniShopApp/MiniShopApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniShopApp.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
